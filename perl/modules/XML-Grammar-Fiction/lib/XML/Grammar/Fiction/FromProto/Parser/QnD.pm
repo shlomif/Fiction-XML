26bfc966a3405edb7df21cefb72ca40d5595ba44
shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2009-11-05 12:37:07 +0000

    Updated the POD URL to the XML-Grammar-Fiction one.

--- perl/modules/XML-Grammar-Fiction/lib/XML/Grammar/Fiction/FromProto/Parser/QnD.pm
+++ perl/modules/XML-Grammar-Fiction/lib/XML/Grammar/Fiction/FromProto/Parser/QnD.pm
@@ -833,7 +833,7 @@ Shlomi Fish, L<http://www.shlomifish.org/>.
 
 Please report any bugs or feature requests to
 C<bug-xml-grammar-fiction at rt.cpan.org>, or through the web interface at
-L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=XML-Grammar-Screenplay>.
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=XML-Grammar-Fiction>.
 I will be notified, and then you'll automatically be notified of progress on
 your bug as I make changes.
 
