c4c20e5a2393e53707e45969469b93d7d6a80b24
shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2009-11-12 11:53:52 +0000

    Extracted the Struct/Tag.pm module with documentation.
    --HG--
    extra : convert_revision : svn%3A52c325ad-5fd0-0310-8a0f-c43feede02cc/XML-Grammar-Fiction/trunk%403966

---
+++ perl/modules/XML-Grammar-Fiction/lib/XML/Grammar/Fiction/Struct/Tag.pm
@@ -0,0 +1,60 @@
+package XML::Grammar::Fiction::Struct::Tag;
+
+use strict;
+use warnings;
+
+use Moose;
+
+
+=head1 NAME
+
+XML::Grammar::Fiction::Struct::Tag - information about an XML/SGML opening or 
+closing tag.
+
+B<For internal use only>.
+
+=head1 VERSION
+
+Version 0.0.1
+
+=cut
+
+our $VERSION = '0.0.1';
+
+has 'name' => (is => "rw", isa => "Str");
+has 'line' => (is => "rw", isa => "Int");
+
+=head1 METHODS
+
+=head2 $self->name()
+
+The tag's name.
+
+=head2 $self->line()
+
+The tag's line number.
+
+=head1 AUTHOR
+
+Shlomi Fish, L<http://www.shlomifish.org/>.
+
+=head1 BUGS
+
+Please report any bugs or feature requests to
+C<bug-xml-grammar-fiction at rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=XML-Grammar-Fiction>.
+I will be notified, and then you'll automatically be notified of progress on
+your bug as I make changes.
+
+=head1 ACKNOWLEDGEMENTS
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2007 Shlomi Fish, all rights reserved.
+
+This program is released under the following license: MIT X11.
+
+=cut
+
+1;
+
