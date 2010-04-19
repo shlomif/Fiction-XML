6ec5a3d2351f6ffddd940205988a9004f77ca73a
shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2010-04-19 14:40:27 +0000

    Extract the XmlIterator class.
    --HG--
    extra : convert_revision : svn%3A52c325ad-5fd0-0310-8a0f-c43feede02cc/XML-Grammar-Fiction/trunk%404291

---
+++ perl/modules/XML-Grammar-Fiction/lib/XML/Grammar/Fiction/FromProto/Parser/XmlIterator.pm
@@ -0,0 +1,90 @@
+package XML::Grammar::Fiction::FromProto::Parser::XmlIterator;
+
+use strict;
+use warnings;
+
+use Moose;
+
+use XML::Grammar::Fiction::Err;
+
+extends("XML::Grammar::Fiction::FromProto::Parser::LineIterator");
+
+has "_tags_stack" => (isa => "ArrayRef", is => "rw", default => sub { [] },);
+
+has "_events_queue" =>
+(
+    isa => "ArrayRef",
+    is => "rw", 
+    default => sub { []; },
+    traits => ['Array'],
+    handles =>
+    {
+        _enqueue_event => 'push',
+        _extract_event => 'shift',
+    },
+);
+
+sub _add_to_top_tag
+{
+    my ($self, $child) = @_;
+
+    $self->_tags_stack->[-1]->append_child($child);
+
+    return;
+}
+
+=head1 NAME
+
+XML::Grammar::Fiction::FromProto::Parser::XmlIterator - line iterator base
+class with some nested XMLisms.
+
+B<For internal use only>.
+
+=cut
+
+our $VERSION = '0.0.4';
+
+=head1 VERSION
+
+Version 0.0.4
+
+=head1 SYNOPSIS
+
+B<TODO:> write one.
+
+=head1 DESCRIPTION
+
+This is a line iterator with some features for parsing, nested, 
+XML-like grammars.
+
+=head1 METHODS
+
+
+=head2 $self->meta()
+
+Leftover from Moose.
+
+=head1 AUTHOR
+
+Shlomi Fish, L<http://www.shlomifish.org/>.
+
+=head1 BUGS
+
+Please report any bugs or feature requests to
+C<bug-xml-grammar-fiction at rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=XML-Grammar-Fiction>.
+I will be notified, and then you'll automatically be notified of progress on
+your bug as I make changes.
+
+=head1 ACKNOWLEDGEMENTS
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2007 Shlomi Fish, all rights reserved.
+
+This program is released under the following license: MIT X11.
+
+=cut
+
+1;
+
