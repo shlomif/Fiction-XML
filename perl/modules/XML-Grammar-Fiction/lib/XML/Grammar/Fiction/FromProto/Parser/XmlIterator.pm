ba0956ea6b1c10285f76ae69b36126d70ffbe8fa
shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2010-04-20 18:38:35 +0000

    Move the common _handle_close_tag functionality to XmlIterator.

--- perl/modules/XML-Grammar-Fiction/lib/XML/Grammar/Fiction/FromProto/Parser/XmlIterator.pm
+++ perl/modules/XML-Grammar-Fiction/lib/XML/Grammar/Fiction/FromProto/Parser/XmlIterator.pm
@@ -177,6 +177,26 @@ sub _merge_tag
     }
 }
 
+sub _handle_close_tag
+{
+    my $self = shift;
+
+    my $close = $self->_parse_closing_tag();
+
+    my $open = $self->_pop_tag();
+
+    if ($open->name() ne $close->name())
+    {
+        XML::Grammar::Fiction::Err::Parse::TagsMismatch->throw(
+            error => "Tags do not match",
+            opening_tag => $open,
+            closing_tag => $close,
+        );
+    }
+
+    return $self->_merge_tag($open);
+}
+
 sub _look_ahead_for_comment
 {
     my $self = shift;
