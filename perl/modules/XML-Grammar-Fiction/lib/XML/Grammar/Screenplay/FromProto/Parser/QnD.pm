fe93dd27a6e689a8a59264203ed46d40c2db444a
shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2010-04-21 21:02:19 +0000

    Removed the functions for real this time - forgot to save the file. :-S.

--- perl/modules/XML-Grammar-Fiction/lib/XML/Grammar/Screenplay/FromProto/Parser/QnD.pm
+++ perl/modules/XML-Grammar-Fiction/lib/XML/Grammar/Screenplay/FromProto/Parser/QnD.pm
@@ -168,48 +168,6 @@ around '_parse_opening_tag' => sub {
     }
 };
 
-sub _parse_inner_tag
-{
-    my $self = shift;
-
-    my $open = $self->_parse_opening_tag();
-
-    if ($open->is_standalone())
-    {
-        # $self->skip_multiline_space();
-
-        return $self->_create_elem($open);
-    }
-
-    my $inside = $self->_parse_inner_text();
-
-    my $close = $self->_parse_closing_tag();
-
-    if ($open->{name} ne $close->{name})
-    {
-        Carp::confess("Opening and closing tags do not match: " 
-            . "$open->{name} and $close->{name} on element starting at "
-            . "line $open->{line}"
-        );
-    }
-    return $self->_create_elem($open, $self->_new_list($inside));
-}
-
-sub _determine_tag
-{
-    my $self = shift;
-
-    my $l = $self->curr_line_ref();
-
-    return
-          ($$l =~ m{\G\[}) ? "open_desc"
-        : ($$l =~ m{\G\&}) ? "entity"
-        : ($$l =~ m{\G(?:</|\])}) ? "close"
-        : ($$l =~ m{\G<}) ? "open_tag"
-        : undef
-        ;
-}
-
 sub _parse_speech_unit
 {
     my $self = shift;
