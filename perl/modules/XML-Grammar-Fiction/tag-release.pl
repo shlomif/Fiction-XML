d201995e85c04553b0a7e97b3a2ef7f36241a6e1
shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2011-04-03 22:02:11 +0000

    Add the tag-release.pl script.

---
+++ perl/modules/XML-Grammar-Fiction/tag-release.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use IO::All;
+
+my ($version) = 
+    (map { m{\$VERSION *= *'([^']+)'} ? ($1) : () } 
+    io->file('lib/XML/Grammar/Fiction.pm')->getlines()
+    )
+    ;
+
+if (!defined ($version))
+{
+    die "Version is undefined!";
+}
+
+my $mini_repos_base = 'https://svn.berlios.de/svnroot/repos/web-cpan/XML-Grammar-Fiction';
+
+my @cmd = (
+    "svn", "copy", "-m",
+    "Tagging the XML-Grammar-Fiction release as $version",
+    "$mini_repos_base/trunk",
+    "$mini_repos_base/tags/releases/$version",
+);
+
+print join(" ", map { /\s/ ? qq{"$_"} : $_ } @cmd), "\n";
+exec(@cmd);
+
