c0029fab2dafaa990e8e2e253838ab6581b69349
shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2011-06-08 15:40:58 +0000

    Add an example to XML-Grammar-Screenplay.
    --HG--
    extra : convert_revision : svn%3A52c325ad-5fd0-0310-8a0f-c43feede02cc/XML-Grammar-Fiction/trunk%404864

--- perl/modules/XML-Grammar-Fiction/lib/XML/Grammar/Screenplay.pm
+++ perl/modules/XML-Grammar-Fiction/lib/XML/Grammar/Screenplay.pm
@@ -114,7 +114,46 @@ character talks.
 
 =head2 EXAMPLES
 
-Examples can be found in the C<t/data> directory, and here:
+=head3 Comprehensive Example
+
+    <s id="top">
+
+    <s id="david_and_goliath">
+
+    [David and <a href="http://en.wikipedia.org/wiki/Goliath">Goliath</a> are 
+    standing by each other.]
+
+    David: I will kill you.
+
+    Goliath: no way, you little idiot!
+
+    David: yes way!
+
+    ++++: In the name of <a href="http://real-allah.tld/">Allah, the 
+    <b>merciful</b>, real merciful</a>, I will show you
+    the [sarcastically] power of my sling. 
+
+    ++: I shall sling you and bing you till infinity.
+
+    [David takes his sling.]
+
+    Goliath: I'm still <a href="http://wait.tld/">waiting</a>.
+
+    David: so you are.
+
+    [David puts a stone in his sling and shoots Goliath. He hits.]
+
+    David: as is written in the wikipedia [See <a href="http://wiki.tld/">the
+    Wiki site</a> for more information], you are now dead, having been shot with
+    my sling.
+
+    </s>
+
+    </s>
+
+=head3 More Examples
+
+Other examples can be found in the C<t/data> directory, and here:
 
 =over 4
 
