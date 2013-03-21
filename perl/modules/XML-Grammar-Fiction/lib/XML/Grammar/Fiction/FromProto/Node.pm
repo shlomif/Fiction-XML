f9e4fddd2cab1fcaa38882144e4cd82ec80dba44
Shlomi Fish <shlomif@shlomifish.org>
2013-03-21 18:17:40 +0200

    Extract lib/XML/Grammar/Fiction/FromProto/Node.pm .

---
+++ perl/modules/XML-Grammar-Fiction/lib/XML/Grammar/Fiction/FromProto/Node.pm
@@ -0,0 +1,41 @@
+package XML::Grammar::Fiction::FromProto::Node;
+
+use strict;
+use warnings;
+
+use List::Util ();
+
+=head1 NAME
+
+XML::Grammar::Fiction::FromProto::Node - contains several nodes for
+use in XML::Grammar::Fiction::FromProto.
+
+=head1 VERSION
+
+Version 0.11.1
+
+=cut
+
+our $VERSION = '0.11.1';
+
+use MooX 'late';
+
+sub _short_isa
+{
+    my $self = shift;
+    my $isa_classish = shift;
+
+    return
+        $self->isa(
+            "XML::Grammar::Fiction::FromProto::Node::$isa_classish"
+        );
+}
+
+1;
+
+=head1 DESCRIPTION
+
+Contains several nodes.
+
+=cut
+
