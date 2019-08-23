d4a38dcf7be7f79b1d0116835a23748464a7c7fb
Shlomi Fish <shlomif@shlomifish.org>
2019-08-23 14:32:18 +0300

    from bitbucket-hg to github due to sunsetting

--- perl/modules/XML-Grammar-Fiction/scripts/tag-release.pl
+++ perl/modules/XML-Grammar-Fiction/scripts/tag-release.pl
@@ -6,22 +6,20 @@ use warnings;
 use IO::All;
 
 my ($version) =
-    (map { m{\$VERSION *= *'([^']+)'} ? ($1) : () }
-    io->file('lib/XML/Grammar/Fiction.pm')->getlines()
-    )
-    ;
+    ( map { m{\Aversion * = *(\S+)} ? ($1) : () }
+        io->file("./dist.ini")->getlines() );
 
-if (!defined ($version))
+if ( !defined($version) )
 {
     die "Version is undefined!";
 }
 
 my @cmd = (
-    "hg", "tag", "-m",
+    "git", "tag", "-m",
     "Tagging the XML-Grammar-Fiction release as $version",
     "releases/XML-Grammar-Fiction/$version",
 );
 
-print join(" ", map { /\s/ ? qq{"$_"} : $_ } @cmd), "\n";
+print join( " ", map { /\s/ ? qq{"$_"} : $_ } @cmd ), "\n";
 exec(@cmd);
 
