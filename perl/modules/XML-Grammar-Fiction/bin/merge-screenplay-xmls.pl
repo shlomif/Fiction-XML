04d9a7c0fb331e8caf2f539153e771be7ef62798
Shlomi Fish <shlomif@shlomifish.org>
2024-11-02 07:06:27 +0200

    Extract a method or a function.
    This is Refactoring / code cleanup.
    
    See:
    
    * https://refactoring.com/catalog/extractMethod.html
    
    * https://en.wikipedia.org/wiki/Code_refactoring
    
    * https://www.refactoring.com/
    
    * https://www.joelonsoftware.com/2002/01/23/rub-a-dub-dub/
    
    Some small optimisations may have slipped in as well.

--- perl/modules/XML-Grammar-Fiction/bin/merge-screenplay-xmls.pl
+++ perl/modules/XML-Grammar-Fiction/bin/merge-screenplay-xmls.pl
@@ -13,36 +13,36 @@ my $docs_dir_obj =
 my $parser = XML::LibXML->new();
 my @sources;
 push @sources,
-    scalar(
-    $parser->parse_file(
-        $docs_dir_obj->child(
-            "Queen-Padme-Tales--Queen-Amidala-vs-the-Klingon-Warriors.xml")
-    )
-    );
+    $docs_dir_obj->child(
+    "Queen-Padme-Tales--Queen-Amidala-vs-the-Klingon-Warriors.xml");
 
-push @sources,
-    scalar(
-    $parser->parse_file(
-        $docs_dir_obj->child("Queen-Padme-Tales--Planting-Trees.xml")
-    )
-    );
+push @sources, $docs_dir_obj->child("Queen-Padme-Tales--Planting-Trees.xml");
 
 my $SCREENPLAY_XML_NS =
 "http://web-cpan.berlios.de/modules/XML-Grammar-Screenplay/screenplay-xml-0.2/";
 
-my $new_xml = $parser->parse_string(
-qq#<document xmlns="$SCREENPLAY_XML_NS"><head></head><body id="index"></body></document>#
-);
-my $root = $new_xml->documentElement();
-foreach my $src (@sources)
+my $output_text = _merge( { inputs => [@sources] } );
+print $output_text;
+
+sub _merge
 {
-    my $doc = $src->documentElement();
-    my $xpc = XML::LibXML::XPathContext->new($doc);
-    $xpc->registerNs( "sp", $SCREENPLAY_XML_NS );
-    my @el = $xpc->findnodes("//sp:document/sp:body/sp:scene");
-    foreach my $el (@el)
+    my $args    = shift;
+    my $inputs  = $args->{inputs};
+    my $new_xml = $parser->parse_string(
+qq#<document xmlns="$SCREENPLAY_XML_NS"><head></head><body id="index"></body></document>#
+    );
+    my $root = $new_xml->documentElement();
+    foreach my $src (@$inputs)
     {
-        $root->appendWellBalancedChunk( $el->toString() );
+        my $input = $parser->parse_file($src);
+        my $doc   = $input->documentElement();
+        my $xpc   = XML::LibXML::XPathContext->new($doc);
+        $xpc->registerNs( "sp", $SCREENPLAY_XML_NS );
+        my @el = $xpc->findnodes("//sp:document/sp:body/sp:scene");
+        foreach my $el (@el)
+        {
+            $root->appendWellBalancedChunk( $el->toString() );
+        }
     }
+    return $new_xml->toString(1);
 }
-print $new_xml->toString(1);
